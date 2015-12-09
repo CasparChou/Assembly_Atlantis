db426028c26c400527b9a3b43d5483b218eba0ae
CasparChou <starcaspar@gmail.com>
2015-12-09 14:31:01 +0800

    Build 0001
    20151209 FIrst Build

---
+++ Atlantis.asm
@@ -0,0 +1,26 @@
+
+
+	INCLUDE	Irvine32.inc
+	INCLUDE	Graph_castle.inc
+	INCLUDE	Graph_soldier.inc
+	INCLUDE	Graph_turret.inc
+	INCLUDE	Motion.inc
+	INCLUDE	UI.inc
+	INCLUDE	Function.inc
+	INCLUDE	Status.inc
+	INCLUDE	Helpers.inc
+
+
+
+
+.Code
+MAIN:
+
+
+
+	Draw
+
+
+
+ExitMain
+END MAIN
