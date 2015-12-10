138aafde1a7f1e69f3e3b8b45fff2cdbbd552fae
CasparChou <starcaspar@gmail.com>
2015-12-10 15:53:35 +0800

    Bulid 0003
    Update 2015 12 10

--- Atlantis.asm
+++ Atlantis.asm
@@ -1,17 +1,15 @@
 
 
 	INCLUDE	Irvine32.inc
+	INCLUDE	Motion.inc
+	INCLUDE	Status.inc
+	INCLUDE	Function.inc
+	INCLUDE	Helpers.inc
+
+	INCLUDE	UI.inc
 	INCLUDE	Graph_castle.inc
 	INCLUDE	Graph_soldier.inc
 	INCLUDE	Graph_turret.inc
-	INCLUDE	Motion.inc
-	INCLUDE	UI.inc
-	INCLUDE	Function.inc
-	INCLUDE	Status.inc
-	INCLUDE	Helpers.inc
-
-
-
 
 .Code
 MAIN:
