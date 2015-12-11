b03d5cbd65693c3b6013c85bab87688383a25395
CasparChou <starcaspar@gmail.com>
2015-12-11 18:19:09 +0800

    Build 0010
    +Beats:
    
    +Beats2x
    +Beats3x
    +Beats5x
    
    To Pulse Object Movment

--- Atlantis.asm
+++ Atlantis.asm
@@ -1,10 +1,11 @@
 
 
 	INCLUDE	Irvine32.inc
+	INCLUDE	Helpers.inc
+	
 	INCLUDE	Motion.inc
 	INCLUDE	Status.inc
 	INCLUDE	Function.inc
-	INCLUDE	Helpers.inc
 
 	INCLUDE	UI.inc
 	INCLUDE	Graph_castle.inc
@@ -12,13 +13,19 @@
 	INCLUDE	Graph_turret.inc
 	INCLUDE	Graph_Monster.inc
 
+	TitleS 	DB	"Atlantis",0
+	HAND	DWORD	?	
+	DWS		COORD <200,200>
 .Code
 MAIN:
-
-	Draw
-	NL
-	NL
-	NL
-
+	LoopTimes 0FFFH
+	DR:
+		Draw
+		PauseTime 1000
+		;CALL ForceCLS
+		;AnyKeyToContinue
+		CALL ClrScr
+		;AnyKeyToContinue
+		NEXT DR
 ExitMain
 END MAIN
