9411cc524b8c49a8ea4fcceb692b3105ebb23e7c
CasparChou <starcaspar@gmail.com>
2015-12-11 19:24:03 +0800

    Build 0011
    *Graph_*.inc [Remove] Y
    +Register
    +Object
    +ObjectList

--- Atlantis.asm
+++ Atlantis.asm
@@ -2,8 +2,8 @@
 
 	INCLUDE	Irvine32.inc
 	INCLUDE	Helpers.inc
-	
-	INCLUDE	Motion.inc
+
+	INCLUDE	Object.inc
 	INCLUDE	Status.inc
 	INCLUDE	Function.inc
 
@@ -12,20 +12,28 @@
 	INCLUDE	Graph_soldier.inc
 	INCLUDE	Graph_turret.inc
 	INCLUDE	Graph_Monster.inc
-
-	TitleS 	DB	"Atlantis",0
-	HAND	DWORD	?	
-	DWS		COORD <200,200>
 .Code
 MAIN:
+	RegisterObjects
 	LoopTimes 0FFFH
 	DR:
-		Draw
+		CLS
+		;Draw
+
+		SettingSolderLV UserSoilders, 2, 3
+		SettingSolderLV UserSoilders, 1, 5
+		SettingSolderLV UserSoilders, 0, 4
+
+		GettingSolderLV UserSoilders, 1, GetterHandler
+		WriteD	GetterHandler
+		NL
+		GettingSolderLV UserSoilders, 0, GetterHandler
+		WriteD	GetterHandler
+		NL
+		GettingSolderLV UserSoilders, 2, GetterHandler
+		WriteD	GetterHandler
+
 		PauseTime 1000
-		;CALL ForceCLS
-		;AnyKeyToContinue
-		CALL ClrScr
-		;AnyKeyToContinue
 		NEXT DR
 ExitMain
 END MAIN
